54f6b84806c7777798264e06df0d849446eb2468
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 02:12:03 -0700

    More flowtype removed

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
+/* eslint-disable flowtype/require-return-type */
 import objectFrom from "./index"
 
 test("works", () => {
