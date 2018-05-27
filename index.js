e58862f2a4fd3f876f41e8b04cc4ea76da85ad6f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 02:12:03 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-import reduceValues from "@unction/reducevalues"
-import attach from "@unction/attach"
-import reverse from "@unction/reverse"
-
-import type {UnaryFunctionType} from "types"
-import type {RecordType} from "types"
-import type {KeyChainType} from "types"
-import type {KeyType} from "types"
-
-export default function objectFrom (keychain: KeyChainType): UnaryFunctionType {
-  return function objectFromKeychain (value: mixed): RecordType {
-    return reduceValues(
-      (accumulated: RecordType): UnaryFunctionType => (key: KeyType): RecordType => attach(key)(accumulated)({})
-    )(
-      value
-    )(
-      reverse(keychain)
-    )
-  }
+import reduceValues from "@unction/reducevalues";
+import attach from "@unction/attach";
+import reverse from "@unction/reverse";
+export default function objectFrom(keychain) {
+  return function objectFromKeychain(value) {
+    return reduceValues(accumulated => key => attach(key)(accumulated)({}))(value)(reverse(keychain));
+  };
 }
