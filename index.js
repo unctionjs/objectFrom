8c9237d7283e93fb642079bf08289dbb6215386a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:44 -0700

    Autolinting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import reduceValues from "@unction/reducevalues";
 import attach from "@unction/attach";
 import reverse from "@unction/reverse";
-export default function objectFrom(keychain) {
-  return function objectFromKeychain(value) {
-    return reduceValues(accumulated => key => attach(key)(accumulated)({}))(value)(reverse(keychain));
+export default function objectFrom (keychain) {
+  return function objectFromKeychain (value) {
+    return reduceValues((accumulated) => (key) => attach(key)(accumulated)({}))(value)(reverse(keychain));
   };
 }
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 /* eslint-disable flowtype/require-return-type */
-import objectFrom from "./index"
+import objectFrom from "./index";
 
 test("works", () => {
   expect(
     objectFrom(["key", "subkey"])("value")
   ).toEqual(
     {key: {subkey: "value"}}
-  )
-})
+  );
+});
